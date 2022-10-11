5129532555b0d0767e27c2b231acce1efb87574b
ariop <ariop@INACAP.CL>
2022-10-11 16:01:36 -0300

    hola

--- movies/movie/models.py
+++ movies/movie/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class Movie(models.Model):
     nombre = models.CharField(max_length=100)
     comentarios = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Director(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre +" "+ self.apellido
--- movies/movie/views.py
+++ movies/movie/views.py
@@ -1,9 +1,15 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from .models import Movie, Director
 # Create your views here.
 
 def home(request):
+    movie_list = Movie.objects.all()
+    context = {
+        "movies": movie_list
+    }
     return render(
         request,
-        'index.html'
+        'index.html',
+        context
     )
