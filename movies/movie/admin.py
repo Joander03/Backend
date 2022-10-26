0414a54dbd1fd60466dabd0110ac9dee96a5d77e
ariop <ariop@INACAP.CL>
2022-10-26 16:02:55 -0300

    ola

--- movies/movie/admin.py
+++ movies/movie/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from movie.models import Director, Movie
+from movie.models import Actor
 
 # Register your models here.
 
 admin.site.register(Movie)
 admin.site.register(Director)
+admin.site.register(Actor)
--- movies/movie/models.py
+++ movies/movie/models.py
@@ -1,3 +1,5 @@
+from statistics import mode
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
@@ -6,7 +8,13 @@ class Movie(models.Model):
     nombre = models.CharField(max_length=100)
     comentarios = models.TextField()
     imdb = models.IntegerField(default=1)
-    Director = models.ForeignKey()
+    director = models.ForeignKey(
+        'Director',
+        on_delete=models.CASCADE
+    )
+    actores = models.ManyToManyField(
+        'Actor'
+    )
     
     def __str__(self):
         return self.nombre
@@ -17,3 +25,12 @@ class Director(models.Model):
 
     def __str__(self):
         return self.nombre +" "+ self.apellido
+
+class Actor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    edad = models.IntegerField()
+
+    def __str__(self):
+        return f''
+
